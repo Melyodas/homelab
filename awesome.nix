2cf48621d1263d86f97ff07fef643c42ff5ea321
Matthieu Moatti <matthieu.moatti@epita.fr>
2025-03-28 12:37:59 +0100

    awesome: add awesome.nix

---
+++ awesome.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+
+{
+  xsession.enable = true;
+
+  xsession.windowManager.awesome = {
+      enable = true;
+      luaModules = with pkgs.luaPackages; [
+          luarocks # is the package manager for Lua modules
+          luadbi-mysql # Database abstraction layer
+      ];
+  };
+}
