dbba83f1163cede87819d099bae81839696c5ff5
Matthieu Moatti <matthieu.moatti@epita.fr>
2025-03-28 13:51:34 +0100

    claptrap: add configuration

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source, using the nixos-24.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.claptrap = nixpkgs.lib.nixosSystem {
+      modules = [
+        ./machines/claptrap
+      ];
+    };
+  };
+}
