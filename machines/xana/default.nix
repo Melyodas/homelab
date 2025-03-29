44e9b92ef1c73e9a8fb9f457ef4e554bf06219bc
Matthieu Moatti <matthieu.moatti@epita.fr>
2025-03-29 16:40:17 +0100

    xana: remove modularised options from config

--- machines/xana/default.nix
+++ machines/xana/default.nix
@@ -9,42 +9,16 @@ in {
     ./grocy.nix
   ];
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  networking = {
-    hostName = "xana";
-  };
-
-  environment.systemPackages = with pkgs; [
-    vim
-    firefox
-  ];
+  networking.hostName = "xana";
 
   services.openssh.enable = true;
 
   programs.nix-ld.enable = true;
 
-  virtualisation.docker.enable = true;
-
   programs.dconf.enable = true;
-
-  users = {
-    users.melyodas = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" "docker" ];
-    };
-  };
 
   # Enable GPU acceleration
   hardware.raspberry-pi."4".fkms-3d.enable = true;
-  services.xserver = {
-    enable = true;
-    desktopManager.xfce.enable = true;
-
-    layout = "fr";
-    xkbVariant = "bepo";
-    xkbOptions = "caps:escape";
-  };
 
   system.stateVersion = "22.11";
 }
