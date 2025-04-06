7607ce01bfbdd24cedd4b7842f0bbb620f4bc37d
Matthieu Moatti <matthieu.moatti@epita.fr>
2025-04-06 23:48:01 +0200

    neovim: plugins: which-key: add bindings

--- home/neovim/plugins/utils/which-key.nix
+++ home/neovim/plugins/utils/which-key.nix
@@ -32,6 +32,29 @@
           ];
         };
         spec = [
+          {
+            __unkeyed-1 = "<leader>p";
+            group = "Project";
+            icon = "󰓩 ";
+          }
+          {
+            __unkeyed-1 = "<leader>pf";
+            __unkeyed-2.__raw = ''
+              function()
+                require('fzf-lua').files()
+              end
+            '';
+            mode = "n";
+            desc = "Open file";
+            silent = true;
+          }
+          {
+            __unkeyed-1 = "<leader>pt";
+            __unkeyed-2 = "<cmd>CHADopen<CR>";
+            mode = "n";
+            desc = "Open Tree";
+            silent = true;
+          }
         ];
         win = {
           border = "single";
